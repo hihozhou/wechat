bdd1a92859a4c80b28b2a150ef7e003ea47b1980
hihozhou <hihozhou@gmail.com>
2019-10-24 19:11:50 +0800

    error

---
+++ error.go
@@ -0,0 +1,18 @@
+package wechat
+
+import (
+	"fmt"
+)
+
+type ApiError struct {
+	ErrCode int64  `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+}
+
+func (a *ApiError) isError() bool {
+	return a.ErrMsg != ""
+}
+
+func (a *ApiError) Error() string {
+	return fmt.Sprintf("Api Error: Code: %d Message: %s", a.ErrCode, a.ErrMsg)
+}
