4699a63b0b55e31436c232fbb1ffda4664aa63b8
hihozhou <hihozhou@gmail.com>
2019-11-07 13:44:56 +0800

    获取令牌

--- component/wechat_component.go
+++ component/wechat_component.go
@@ -1,11 +1,26 @@
 package component
 
 import (
+	"fmt"
 	"github.com/hihozhou/wechat/cache"
+	"github.com/hihozhou/wechat/http"
 	"time"
 )
 
 const VerifyTicketCacheKeyPrefix = "wechat:component:verify_ticket:"
+
+var (
+	weixinComponentHost       = "https://api.weixin.qq.com/cgi-bin/component"
+	apiComponentTokenUrl      = weixinComponentHost + "/api_component_token"
+	apiCreatePreAuthCodeUrl   = weixinComponentHost + "/api_create_preauthcode?component_access_token=%s"
+	apiQueryAuthUrl           = weixinComponentHost + "/api_query_auth?component_access_token=%s"
+	apiAuthorizerTokenUrl     = weixinComponentHost + "/api_authorizer_token?component_access_token=%s"
+	apiGetAuthorizerInfoUrl   = weixinComponentHost + "/api_get_authorizer_info?component_access_token=%s"
+	apiGetAuthorizerOptionUrl = weixinComponentHost + "/api_get_authorizer_option?component_access_token=%s"
+	apiSetAuthorizerOptionUrl = weixinComponentHost + "/api_set_authorizer_option?component_access_token=%s"
+
+	oauthUrl = "https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=%s&pre_auth_code=%s&redirect_uri=%s"
+)
 
 type WechatComponent struct {
 	AppId          string //appId
@@ -46,11 +61,28 @@ func (wc *WechatComponent) GetVerifyTicket() (string, bool) {
 	return "", exist
 }
 
-// 请求验证
-
-func GetAccessToken() {
+// 获取令牌component_access_token
+// author:hihozhou
+// 文档https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/api/component_access_token.html
+func (wc *WechatComponent) GetAccessToken() {
 	//http client
 	//http 请求
+	ticket, exist := wc.GetVerifyTicket()
+	if !exist {
+		//返回错误
+	}
+	postData := struct {
+		Component_appid         string `json:"component_appid"`
+		Component_appsecret     string `json:"component_appsecret"`
+		Component_verify_ticket string `json:"component_verify_ticket"`
+	}{
+		Component_appid:         wc.AppId,
+		Component_appsecret:     wc.AppSecret,
+		Component_verify_ticket: ticket,
+	}
+	result := http.Post(apiComponentTokenUrl, postData, "application/json")
+	fmt.Println(result)
+
 	//返回结果
 	//储存token
 	//appId := "wxdc4e3ad21308ea2a"
---
+++ http/client.go
@@ -0,0 +1,55 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func Get(url string) string {
+	client := http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var buffer [512]byte
+	result := bytes.NewBuffer(nil)
+	for {
+		n, err := resp.Body.Read(buffer[0:])
+		result.Write(buffer[0:n])
+		if err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+	}
+
+	return result.String()
+}
+
+// 发送POST请求
+// url:请求地址，data:POST请求提交的数据,contentType:请求体格式，如：application/json
+// content:请求放回的内容
+func Post(url string, data interface{}, contentType string) string {
+	jsonStr, _ := json.Marshal(data)
+	req, err := http.NewRequest(`POST`, url, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add(`content-type`, contentType)
+	defer req.Body.Close()
+
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	result, _ := ioutil.ReadAll(resp.Body)
+	return string(result)
+}
